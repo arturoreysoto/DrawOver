224325fde4eeca58b96c14289cd432ba64011c4e
Arturo Rey <arturoreysoto@gmail.com>
2026-04-09 11:00:26 +0200

    Initial Commit

---
+++ SketchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SketchApp
+//
+//  Created by Arturo Rey Soto on 09/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SketchApp/SketchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchAppApp.swift
+//  SketchApp
+//
+//  Created by Arturo Rey Soto on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SketchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
